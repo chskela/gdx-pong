ec8ae7bb54e5c17302851fe8808236eee4e1e982
Chistaykov Aleksey <chskela@gmail.com>
2022-01-09 14:01:58 +0300

    init

---
+++ android/src/com/chskela/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chskela.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chskela.game.Pong;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Pong(), config);
+	}
+}
---
+++ desktop/src/com/chskela/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chskela.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chskela.game.Pong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pong(), config);
+	}
+}
